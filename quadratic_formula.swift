a4d0f6eb2348f9b5b472daf166155e30cfb2f1bd
MuhammedSultanov <9073@holbertonstudents.com>
2025-03-07 15:17:10 +0400

    quadratic formula

---
+++ quadratic_formula.swift
@@ -0,0 +1,11 @@
+var a: Double = 3
+var b: Double = -11
+var c: Double = -4
+var root1: Double
+var root2: Double
+var d: Double
+d = b*b - 4*a*c
+root1 = (-b + d.squareRoot())/(2*a)
+root2 = (-b - d.squareRoot())/(2*a)
+print(root1)
+print(root2)
